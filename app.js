a25f9e6bc0a4938d1d49cf98dd23099c78260c93
dsound-zz <demiansims@gmail.com>
2020-07-07 20:01:09 -0400

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=1256abb683ee2c67c9183bacff52a85f&query=37.8626,-122.4233'
+const geocode = require('./utils/geocode')
+// const forecast = require('./utils/forecast')
 
+geocode('ldskjfasdlkfjds;alkfjd;lkj', (error, data)=> {
+    console.log('Error', error) 
+    console.log('Data', data)
+});
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
-})
---
+++ utils/forecast.js
@@ -0,0 +1,6 @@
+const request = require('request')
+
+const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGVtaWFuc2ltcyIsImEiOiJja2Nhd3pkNTQwbzd4MnRxam5jNGlkcXVoIn0.DpBcAgsdIQXWI70hMFHZvQ&limit=1";
+
+
+module.exports = forecast;
